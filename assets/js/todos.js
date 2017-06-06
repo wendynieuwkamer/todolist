50f0a3ff51d012a16279281143ddabe771fb6ac8
Wendy Nieuwkamer <wendy.nieuwkamer@gmail.com>
2017-06-06 16:44:17 +0200

    Creating a new list item, not functional

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,27 @@
-// Check off todos by clicking
+// Check off/on todos by clicking
 $("li").click(function() {
-	$(this).toggleClass("done");
+	$(this).toggleClass("completed");
 })
+
+// Click on deleteButton to delete todo
+$(".delete-button").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+// Create a new todo through the input field
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13) {
+		var text = $(this).val();
+		$("#todo-list").append(createNewTodo(text));
+		$(this).val("");
+	}
+})
+
+function createNewTodo (text) {
+	var base = "<li><span>X</span> " ;
+	var end = "</li>" ;
+	return base + text + end
+}
